2a60ff93eddfc87c58115eb1b9db6c2f5d8d968d
sefagvn <sefaguveen@gmail.com>
2017-01-19 14:20:10 +0200

    randevu sayfası tasarlandı

--- app/src/main/java/com/sourcey/materiallogindemo/Randevu.java
+++ app/src/main/java/com/sourcey/materiallogindemo/Randevu.java
@@ -12,10 +12,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Fragment3 extends Fragment {
+public class Randevu extends Fragment {
 
 
-    public Fragment3() {
+    public Randevu() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+        return inflater.inflate(R.layout.fragment_randevu, container, false);
     }
 
 }
